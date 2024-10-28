778ec40901e8114434b65042c6ebb5196b975271
thenewforktimes <robert.w.ballard@gmail.com>
2024-10-28 12:13:43 -0700

    Added time / FPS caps

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from constants import *
 def main():
     #init state
     pygame.init()
+    #init FPS boundaries / time
+    MAX_FRAME_RATE = 60
+    CLOCK = pygame.time.Clock()
+    STABILIZE_FPS = CLOCK.tick(MAX_FRAME_RATE) / 1000
+    #delta time variable
+    DT = 0
     #set and define screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #start game loop
@@ -16,6 +22,7 @@ def main():
         pygame.Surface.fill(screen, (0,0,0))
         #flip screen
         pygame.display.flip()
-           
+        DT = STABILIZE_FPS
+        
 if __name__ == "__main__":
     main()
